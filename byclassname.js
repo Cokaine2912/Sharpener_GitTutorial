780a238ed64246051698cd67a5a1b12441b2a6a2
Cokaine2912 <niraj28352@gmail.com>
2023-12-19 10:42:42 +0530

    ByClassName_pushed

---
+++ byclassname.js
@@ -0,0 +1,8 @@
+// Write your code below:
+const parchi = document.getElementsByClassName('fruit');
+
+parchi[2].style.backgroundColor = 'yellow'
+
+for (var i = 0 ; i < parchi.length ; i ++ ) {
+    parchi[i].style.fontWeight = "bold"
+}
